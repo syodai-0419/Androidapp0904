51dca89d52339cba1b976d992925b20232171be4
Syodai Hasegawa <221205128@ccmailg.meijo-u.ac.jp>
2023-09-05 13:40:26 +0900

    many changed.

--- app/src/main/java/jp/co/meijou/android/s221205128/MainActivity.java
+++ app/src/main/java/jp/co/meijou/android/s221205128/MainActivity.java
@@ -3,12 +3,54 @@ package jp.co.meijou.android.s221205128;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.co.meijou.android.s221205128.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main);
+        //binding.text.setText(R.string.text);
+
+        binding.button.setOnClickListener(view -> {
+            binding.text.setText(R.string.meijo_u);
+        });
+
+        binding.button2.setOnClickListener(view -> {
+            binding.imageView2.setImageResource(R.drawable.seat_flat_angled);
+        });
+
+        binding.button3.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+            }
+        });
     }
+
 }
