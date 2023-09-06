a8458b09a36372c330a8219e4976cf915e355677
Syodai Hasegawa <221205128@ccmailg.meijo-u.ac.jp>
2023-09-06 16:32:28 +0900

    intent activity_main3.xmlと activity_sub.xml間でのデータ送受信

--- app/src/main/java/jp/co/meijou/android/s221205128/MainActivity3.java
+++ app/src/main/java/jp/co/meijou/android/s221205128/MainActivity3.java
@@ -1,15 +1,41 @@
 package jp.co.meijou.android.s221205128;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import java.util.Optional;
 
 import jp.co.meijou.android.s221205128.databinding.ActivityMain3Binding;
 import jp.co.meijou.android.s221205128.databinding.ActivityMainBinding;
 
 public class MainActivity3 extends AppCompatActivity {
     private ActivityMain3Binding binding;
+
+    private final ActivityResultLauncher<Intent> getActivityResult =registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()){
+                    case RESULT_OK:
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result:" + text)
+                                .ifPresent(text -> binding.textViewResult.setText(text));
+                        break;
+                    case RESULT_CANCELED:
+                        binding.textViewResult.setText("Result: Canceled");
+                        break;
+                    default:
+                        binding.textViewResult.setText("Result: Unknown(" + result.getResultCode() + ")");
+                        break;
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +47,24 @@ public class MainActivity3 extends AppCompatActivity {
         binding.buttonMei.setOnClickListener(view ->{
             var intent = new Intent(this, SubActivity.class);
             startActivity(intent);
+        });
+
+        binding.buttonAn.setOnClickListener(view ->{
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
+        });
+
+        binding.buttonSend.setOnClickListener(view ->{
+            var intent =  new Intent(this, SubActivity.class);
+            intent.putExtra("text", binding.editTextData.getText().toString());
+            startActivity(intent);
+        });
+
+        binding.buttonLaunch.setOnClickListener(view ->{
+            var intent = new Intent(this, SubActivity.class);
+            getActivityResult.launch(intent);
         });
     }
 }
--- app/src/main/java/jp/co/meijou/android/s221205128/SubActivity.java
+++ app/src/main/java/jp/co/meijou/android/s221205128/SubActivity.java
@@ -2,7 +2,10 @@ package jp.co.meijou.android.s221205128;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Optional;
 
 import jp.co.meijou.android.s221205128.databinding.ActivitySubBinding;
 
@@ -12,11 +15,28 @@ public class SubActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
 
         binding = ActivitySubBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Optional.ofNullable(getIntent().getStringExtra("text"))
+                        .ifPresent(text -> binding.textView.setText(text));
+        /*
+        これでも書ける
+        var text = getIntent().getStringExtra("text")
+        binding.textView.setText(text);
+         */
 
+        binding.buttonOk.setOnClickListener(view ->{
+            var intent = new Intent();
+            intent.putExtra("ret", "meijo");
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        binding.buttonCancel.setOnClickListener(view ->{
+            setResult(RESULT_CANCELED);
+            finish();
+        });
     }
 }
